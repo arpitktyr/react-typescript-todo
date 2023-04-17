9785bcf6ae708823ad27e75b2b24b79e86f81dd1
arpitktyr <arpitktyr@gmail.com>
2023-04-17 09:33:41 +0530

    delete-btn added

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import classes from './TodoItem.module.css';
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{ text: string;onRemoveTodo:(event:React.MouseEvent)=>void}> = (props) => {
+  return <li className={classes.item} >
+    {props.text}
+    <button className={classes.deleteBtn} onClick={props.onRemoveTodo} > Delete </button>
+    </li>;
 };
 
 export default TodoItem;
